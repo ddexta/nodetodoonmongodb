c2912452c429fab809bc48920ef0bb7e5d638c0a
ddexta <18assdd@gmail.com>
2018-07-21 15:29:44 +0100

    added test for get route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,19 @@ const {app}=require('./../server')
 const {Todo}=require('./../modules/todo')
 const {User}=require('./../modules/user')
 
+const todos=[
+    {
+        text:'first test todo'
+    },
+    {
+        text:'second test todo'
+    }
+]
 
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done())
+    Todo.remove({}).then(()=>{
+        return Todo.insertMany(todos)
+    }).then(()=>done())
 })
 
 describe('post todos',()=>{
@@ -25,7 +35,7 @@ describe('post todos',()=>{
                 if(err){
                     return done(err)
                 }
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     done()
@@ -42,9 +52,20 @@ describe('post todos',()=>{
                     return done(err)
                 }
                 Todo.find().then((todos)=>{
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done()
                 }).catch(e=>done(e))
             })
     })
+})
+describe('get todo',()=>{
+    it('should get all todos',(done)=>{
+        requestOn(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todos.length).toBe(2)    
+            })
+            .end(done)
+})
 })
