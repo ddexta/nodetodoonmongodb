b6215544d369831c43d24723948f8b102c501eef
ddexta <18assdd@gmail.com>
2018-07-24 00:17:50 +0100

    added login

--- server/server.js
+++ server/server.js
@@ -99,7 +99,17 @@ app.get('/users/me',authenticate,(req,res)=>{
     res.send(req.user)
 })
 
+app.post('/users/login',(req,res)=>{
+    let body=_.pick(req.body,['email','password'])
+    User.findByCredentials(body.email,body.password)
+        .then((user)=>{
+            user.generateAuthToken().then((token)=>{
+                res.header('x-auth',token).send(user)
+            })
+        })
+        .catch(err=>res.status(400).send(err))
 
+})
 app.listen(port,()=>console.log(`started server on ${port}`))
 
 module.exports={
